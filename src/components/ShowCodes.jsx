1a6249b1d2dddd648b4df6d5748e0c3b95c509a9
nonnhjleno <taaaisei999@gmail.com>
2023-08-27 20:55:18 +0900

    前回と同じ理由のコミット

--- src/components/ShowCodes.jsx
+++ src/components/ShowCodes.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Octokit } from "@octokit/core";
 
-const ACCESS_TOKEN = "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";
+const ACCESS_TOKEN = "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";
 const octokit = new Octokit({ auth: ACCESS_TOKEN });
 
 function ShowCodes(props) {
