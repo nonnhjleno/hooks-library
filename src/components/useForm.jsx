c2103012d03f2ab7dd117055460873ed4b209a95
nonnhjleno <taaaisei999@gmail.com>
2023-11-06 14:44:58 +0900

    初期値で最初の列の数を制御できるuseFormを使ったform作成

--- src/components/useForm.jsx
+++ src/components/useForm.jsx
@@ -1,22 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
 
 const FormFunc = () => {
-    const { control, handleSubmit } = useForm();
+
+    const count = 3;
+
+    let initialValue = [];
+
+    for (let i = 0; i < count; i++) {
+        initialValue = [...initialValue, { name: "", age: "" }];
+    }
+
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            people: [...initialValue]
+        }
+    });
+
     const { fields, append, remove } = useFieldArray({
         control,
         name: "people",
     });
 
+    const [formCount, setFormCount] = useState(fields.length); // フォームの数を保存する状態変数
+
     const onSubmit = (data) => {
         console.log(data);
+        console.log(fields);
+    };
+
+    const addForm = () => {
+        append({ name: "", age: "" });
+        setFormCount(formCount + 1); // フォームを追加したらフォームの数を更新
+    };
+
+    const deleteForm = (index) => {
+        remove(index);
+        setFormCount(formCount - 1); // フォームを削除したらフォームの数を更新
     };
 
     return (
         <div className="border-black">
             <form onSubmit={handleSubmit(onSubmit)}>
-                <button className=" border-2" type="button" onClick={() => append({ name: "", age: "" })}>
+                <button className="border-2" type="button" onClick={addForm}>
                     追加
                 </button>
                 {fields.map((item, index) => (
@@ -24,22 +52,21 @@ const FormFunc = () => {
                         <Controller
                             name={`people[${index}].name`}
                             control={control}
-                            defaultValue={item.name}
-                            render={({ field }) => <input {...field} placeholder="名前" className="border-2 mx-2"/>}
+                            render={({ field }) => <input {...field} placeholder="名前" className="border-2 mx-2" />}
                         />
                         <Controller
                             name={`people[${index}].age`}
                             control={control}
-                            defaultValue={item.age}
-                            render={({ field }) => <input {...field} placeholder="年齢" className="border-2 mx-2"/>}
+                            render={({ field }) => <input {...field} placeholder="年齢" className="border-2 mx-2" />}
                         />
-                        <button type="button" onClick={() => remove(index)}>
+                        <button type="button" onClick={() => deleteForm(index)}>
                             削除
                         </button>
                     </div>
                 ))}
                 <button type="submit">送信</button>
             </form>
+            <p>フォームの数: {formCount}</p>
         </div>
     );
 };
